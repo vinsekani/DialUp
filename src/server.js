69e3ad316d1afecba6741678352b88f9156f5752
vinsekani <vinsekani@gmail.com>
2025-01-22 11:41:17 +0300

    installed cors

--- src/server.js
+++ src/server.js
@@ -1,32 +1,35 @@
-const express = require('express');
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-const contactRoutes = require ("./routes/contact")
-const authRoutes = require ("./routes/auth")
-const categoryRoutes = require ("./routes/category")
-dotenv.config()
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const contactRoutes = require("./routes/contact");
+const authRoutes = require("./routes/auth");
+const categoryRoutes = require("./routes/category");
+dotenv.config();
 const app = express();
-const PORT =3000;
+const PORT = 8000 || process.env.PORT;
 
-app.get("/",(req,res)=>{
-    // res.send("DialUp, Streamline your contacts with sekani only")
-    res.json({"message":"DialUp, Streamline your contacts with sekani only"})
-})
+app.get("/", (req, res) => {
+  // res.send("DialUp, Streamline your contacts with sekani only")
+  res.json({ message: "DialUp, Streamline your contacts with sekani only" });
+});
 
 // READING JSON
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(cors);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // API ROUTES
-app.use("/api/contacts", contactRoutes)
-app.use("/api/auth", authRoutes)
-app.use("/api/category", categoryRoutes)
+app.use("/api/contacts", contactRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/category", categoryRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    app.listen(PORT,() =>{
-        console.log(`Server is running on http://localhost:${PORT} and DB is connected`)
-    })
-})
-.catch((err) => console.log(err))
-
-
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(
+        `Server is running on http://localhost:${PORT} and DB is connected`
+      );
+    });
+  })
+  .catch((err) => console.log(err));
