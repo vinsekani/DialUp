7764b7002e75685500d2b6816a5cce5e72866904
vinsekani <vinsekani@gmail.com>
2025-01-23 09:52:01 +0300

    add default image url to photo

--- src/models/contact.js
+++ src/models/contact.js
@@ -4,7 +4,7 @@ const contactSchema = new mongoose.Schema({
   name: { type: String, require },
   phone: { type: String, require },
   email: { type: String, require },
-  photo: { type: String, require },
+  photo: { type: String, require, default:"https://res.cloudinary.com/oroko/image/upload/v1737614653/user_pigxco.jpg" },
   category: { type: String },
   company: { type: String },
 },{timestamps:true});
