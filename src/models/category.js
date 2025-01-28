474de5c646d91c0dc4e4b507929070a57f46b34d
vinsekani <vinsekani@gmail.com>
2025-01-28 10:59:48 +0300

    adde delete from the ui but not from data base

--- src/models/category.js
+++ src/models/category.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const categorySchema = new mongoose.Schema({
     name:{type: String, require},
     description:{type:String, require},
-    isDeleted: {type:Boolean}, default:false
+    isDeleted: { type: Boolean, default: false },
 }, {timestamps:true})
 
 module.exports = mongoose.model("Category", categorySchema)
